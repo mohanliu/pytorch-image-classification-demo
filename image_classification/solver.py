c982d2257725a8ee4ac35672303bcc7c8e0c7153
mliu3 <mliu3@cccis.com>
2021-01-07 16:42:23 -0600

    update device setups

--- image_classification/solver.py
+++ image_classification/solver.py
@@ -37,17 +37,14 @@ class Solver(Config):
         
     def _set_device(self, gpu_number):
         if isinstance(gpu_number, int):
-            self._device = torch.device("cuda:{}".format(gpu_number) if torch.cuda.is_available() else "cpu")
-            self._multi_gpu_mode = False
-            self._num_gpu = 1
-            logger.info("Using single device: {}".format(self._device))
+            gpu_number_list = [gpu_number]
+        else:
+            gpu_number_list = gpu_number
 
-        elif isinstance(gpu_number, list):
-            os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, gpu_number))
-            self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            self._multi_gpu_mode = True
-            self._num_gpu = len(gpu_number)
-            logger.info("Using {} GPUs: {}".format(torch.cuda.device_count(), str(gpu_number)))
+        os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, gpu_number_list))
+        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._num_gpu = len(gpu_number_list)
+        logger.info("Using {} GPUs: {}".format(torch.cuda.device_count(), str(gpu_number_list)))
 
     def _get_dataset(self):
         for p in ["train", "val"]:
@@ -55,16 +52,10 @@ class Solver(Config):
             setattr(self, "{}_dataloader".format(p), temp_d.dataloader)
             setattr(self, "{}_datasize".format(p), temp_d._size)
 
-            if p == "train":
-                self.classes = temp_d._classes
-
     def _get_or_load_model(self):
-        self.model = FineTuneModel().get_model(len(self.classes))
-        if self._multi_gpu_mode:
-            self.model = nn.DataParallel(self.model)
-            self.model.to(self._device)
-        else:
-            self.model.to(self._device)
+        self.model = FineTuneModel().get_model(len(self._target_classes))
+        self.model = nn.DataParallel(self.model)
+        self.model.to(self._device)
 
     def _set_optimizer(self, parameters):
         self.optimizer = optim.SGD(
@@ -117,7 +108,7 @@ class Solver(Config):
         else:
             model_fullpath = os.path.join(self._results_folder, resultname)
         
-        logger.info("Lodding model: {}".format(model_fullpath))
+        logger.info("Loading model: {}".format(model_fullpath))
         checkpoint = torch.load(model_fullpath, map_location=self._device)
         self.model.load_state_dict(checkpoint["state_dict"])
         self.model.to(self._device)
