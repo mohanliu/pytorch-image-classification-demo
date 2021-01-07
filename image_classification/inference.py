f39b7dda60556c742d7b370673c5bc324c60cfb5
mliu3 <mliu3@cccis.com>
2021-01-07 16:41:53 -0600

    finalize inference code

--- image_classification/inference.py
+++ image_classification/inference.py
@@ -2,6 +2,7 @@ from __future__ import print_function, division
 
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 import numpy as np
 from torchvision import datasets, models, transforms
 import time
@@ -10,56 +11,96 @@ import copy
 import gc
 import shutil
 import logging
+from PIL import Image
 
 from .config import Config
-from .dataset import FTDataLoader
 from .model import FineTuneModel
 
 logger = logging.getLogger("image_classification.inference")
 
 class ImageClassification(Config):
-    def __init__(self, gpu_number=0, **kwargs):
+    def __init__(self, weight_path, gpu_number=0, **kwargs):
         super().__init__(**kwargs)
         
         # get device
         self._set_device(gpu_number)
-
-        # prepare data
-        self._get_dataset()
         
         # prepare model
-        self._get_or_load_model()
+        self._load_model_weights(weight_path)
         
     def _set_device(self, gpu_number):
         if isinstance(gpu_number, int):
-            self._device = torch.device("cuda:{}".format(gpu_number) if torch.cuda.is_available() else "cpu")
-            self._multi_gpu_mode = False
-            self._num_gpu = 1
-            logger.info("Using single device: {}".format(self._device))
-
-        elif isinstance(gpu_number, list):
-            os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, gpu_number))
-            self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            self._multi_gpu_mode = True
-            self._num_gpu = len(gpu_number)
-            logger.info("Using {} GPUs: {}".format(torch.cuda.device_count(), str(gpu_number)))
-            
-    def _preprocess_data(self, image_dict):
-        for p in ["train", "val"]:
-            temp_d = FTDataLoader(phase=p, global_batch_size=self._num_gpu * self._batch_size)
-            setattr(self, "{}_dataloader".format(p), temp_d.dataloader)
-            setattr(self, "{}_datasize".format(p), temp_d._size)
-
-            if p == "train":
-                self.classes = temp_d._classes
-
-    def _get_or_load_model(self):
-        self.model = FineTuneModel().get_model(len(self.classes))
-        if self._multi_gpu_mode:
-            self.model = nn.DataParallel(self.model)
-            self.model.to(self._device)
+            gpu_number_list = [gpu_number]
         else:
-            self.model.to(self._device)
+            gpu_number_list = gpu_number
+
+        os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, gpu_number_list))
+        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._num_gpu = len(gpu_number_list)
+        logger.info("Using {} GPUs: {}".format(torch.cuda.device_count(), str(gpu_number_list)))
             
-    def predict(self, image_dict):
-        return
+    def _preprocess_data(self, image_path):
+        inference_transforms = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+        
+        image_ = Image.open(image_path).convert('RGB')
+        
+        image_tensor = inference_transforms(image_).float().unsqueeze_(0)
+    
+        return image_tensor
+    
+    def _process_output(self, image_tensor):
+        input_ = image_tensor.to(self._device)
+        output_ = self.model(input_)
+        
+        raw_output = [
+            np.round(v, 4) 
+            for v in output_.data.cpu().numpy().tolist()[0]
+        ]
+        
+        _, preds = torch.max(output_, 1)
+        
+        pred_index = preds.data.cpu().numpy()[0]
+        
+        pred_class = [
+            k 
+            for k, v in self._target_class_to_idx.items()
+            if v == pred_index 
+        ][0]
+        
+        return {
+            "predicted_class": pred_class,
+            "raw_output": raw_output,
+            "predicted_label": pred_index
+        }
+
+    def _load_model_weights(self, weight_path):
+        logger.info("Preparing model: {} ...".format(self._model_backbone))
+        self.model = FineTuneModel().get_model(len(self._target_classes))
+        
+        logger.info("Preparing model: mapping to devices...")
+        self.model = nn.DataParallel(self.model)
+        self.model.to(self._device)
+        
+        logger.info("Loading weights: {} ...".format(weight_path))  
+        checkpoint = torch.load(weight_path, map_location=self._device)
+        
+        self.model.load_state_dict(checkpoint["state_dict"])
+        self.model.to(self._device)
+        
+        logger.info("Model is ready!")
+        
+        self.model.eval()
+        
+            
+    def predict(self, image_path):
+        image_tensor_ = self._preprocess_data(image_path)
+        output_ = self._process_output(image_tensor_)
+        
+        output_.update({"image_path": image_path})
+        
+        return output_
